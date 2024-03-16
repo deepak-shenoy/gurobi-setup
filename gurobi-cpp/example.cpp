3c178f177db7753c48c3cf8cf04409d7686ccdf7
Deepak Shenoy <shenoyd@jupiter.local>
2024-03-16 17:13:57 -0400

    Documenation

--- gurobi-cpp/example.cpp
+++ gurobi-cpp/example.cpp
@@ -1,3 +1,13 @@
+/* Copyright 2024, Gurobi Optimization, LLC */
+
+/* This example formulates and solves the following simple MIP model:
+
+     maximize    x +   y + 2 z
+     subject to  x + 2 y + 3 z <= 4
+                 x +   y       >= 1
+                 x, y, z binary
+*/
+
 #include <iostream>
 #include "gurobi_c++.h"
 
